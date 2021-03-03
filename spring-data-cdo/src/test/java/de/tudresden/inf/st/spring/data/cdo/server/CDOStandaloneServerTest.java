6241ea75cb1b52a265e0c63850c401599fe9a47a
Dominik Grzelak <dominik.grzelak@tu-dresden.de>
2021-03-03 20:35:34 +0100

    - new deployment structure - CDO standalone server test splitted

--- spring-data-cdo/src/test/java/de/tudresden/inf/st/spring/data/cdo/server/CDOStandaloneServerTest.java
+++ spring-data-cdo/src/test/java/de/tudresden/inf/st/spring/data/cdo/server/CDOStandaloneServerTest.java
@@ -1,15 +1,27 @@
 package de.tudresden.inf.st.spring.data.cdo.server;
 
 import de.tudresden.inf.st.spring.data.cdo.CDOStandaloneServer;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 
+@Disabled
 public class CDOStandaloneServerTest {
 
     @Test
     void run_server_test_01() {
         CDOStandaloneServer server = new CDOStandaloneServer("repo1");
-//        CDOStandaloneServer server = new CDOStandaloneServer(new File("spring-data-cdo/src/test/resources/config/cdo-server2.xml"));
+        try {
+            CDOStandaloneServer.start(server);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void run_server_test_02() {
+        CDOStandaloneServer server = new CDOStandaloneServer(new File("src/test/resources/config/cdo-server2.xml"));
         try {
             CDOStandaloneServer.start(server);
         } catch (Exception e) {
